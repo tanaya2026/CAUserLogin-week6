34748860647aa3d6a5083b381ba25ac6854893aa
Tanaya <tanaya.datar@mail.utoronto.ca>
2024-10-20 10:34:00 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current User.
+     * @param name the username to set.
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Returns the user with the given username.
+     * @return the user with the given username
+     */
+    String getCurrentUser();
 }
